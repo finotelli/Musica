a2b0a28c23a1d0646b3a8c8fe7b037b8125da4c7
Ana Finotelli <finotelli2004@gmail.com>
2022-09-02 22:14:54 -0300

    Musica

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("E não me agrada mais");
+Console.WriteLine("Não, não me agrada");
+Console.WriteLine("A simetria é tão sem graça e muito tosca");
+Console.WriteLine("E quero me jogar nesse azul, no infinito desses braços");
+Console.WriteLine("Pois aqui me sinto livre, eu aqui me sinto em casa");
+Console.WriteLine("Eu aqui me sinto inteiro");
